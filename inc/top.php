addbf24b2f823032401fb53229ea6d2cf7bc83e5
Hamzshaa <trident32000@gmail.com>
2024-03-27 21:34:53 +0300

    file, jQuery and bootstrap setup finished

---
+++ inc/top.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  
+    <link href="images/myLogo.png" rel="icon" type="image/png">
+    <Link rel="stylesheet" href="css/bootstrap.min.css" >
+    <Link rel="stylesheet" href="css/custom.css" >
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300, 400" rel="stylesheet">
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+
+    <style>
+        body {
+            font-family: "Raleway", sans-serif;
+        }
+    </style>
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
---
+++ index.php
@@ -0,0 +1,6 @@
+
+<?php
+    include("inc/top.php")
+?>
+
+<button class="btn btn-primary">Hello</button>
