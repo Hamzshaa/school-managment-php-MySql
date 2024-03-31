4a0c8f2e11e0a51aabff0f8b3fb7f89bbdab6e19
Hamzshaa <trident32000@gmail.com>
2024-03-31 11:15:00 +0300

    About us page created

---
+++ about-us.php
@@ -0,0 +1,54 @@
+
+<?php
+    include("inc/top.php")
+?>
+
+<div class="container-fluid px-0">
+    <div class="row">
+        <div class="col-lg-12">
+            <?php include("inc/navbar.php") ?>
+        </div>
+    </div>
+
+    <div class="row mt-2">
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header bg-primary text-white">
+                    About Trident Academy
+                </div>
+                <div class="card-body text-justify">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse non expedita iste fuga dignissimos aspernatur consequuntur commodi, neque sequi saepe obcaecati, eos ut numquam dolor praesentium, ullam mollitia in? Architecto laudantium, nisi dolorum ut doloribus beatae nam. Eveniet, repellat corrupti?
+                    </p>
+                    <p class="card-text">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe minima illo ullam assumenda minus quaerat cupiditate alias. Officiis sequi voluptatibus culpa eaque architecto tenetur dolores odio mollitia veritatis quos ex, consequatur animi neque ab. Sunt inventore eaque unde minima at? Repellendus, velit id! Ratione nam eius tenetur alias neque omnis quia dolores quae repellat, velit adipisci? Autem provident eos modi.
+                    </p>
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur animi repellendus praesentium autem maiores iusto quod corrupti, deserunt magni pariatur, aliquid voluptate facilis molestias?
+                    </p>
+                    <a href="gallery.php" class="btn btn-danger">View Gallery</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card bg-warning text-white">
+                <div class="card-body">
+                    <h4 class="card-title text-center">Parents Review</h4>
+                </div>
+                
+            </div>
+            <img src="images/18.jpg" alt="" class="img-fluid">
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row bg-dark mt-2">
+            <?php include("inc/footer.php") ?>
+        </div>
+    </div>
+</div>
+
+
+
+ </body>
+</html>
