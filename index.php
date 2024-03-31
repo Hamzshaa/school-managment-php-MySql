8ad3cdfc1d1acdb9aa005a2e111164a10fac470a
Hamzshaa <trident32000@gmail.com>
2024-03-31 11:03:13 +0300

    sections in main page added

--- index.php
+++ index.php
@@ -11,6 +11,80 @@
         </div>
     </div>
 
+    <div class="row mt-2">
+        <div class="col-md-9">
+            <h2 class="bg-dark text-white text-center">WELCOME TO TRIDENT ACADEMY</h2>
+            <p class="text-justify"><b class="text-danger">Trident Academy </b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque esse inventore nulla accusantium asperiores ea error, optio maxime, at doloribus placeat nihil assumenda adipisci laudantium deleniti eos dolores corrupti fuga perferendis. Quo cumque dignissimos eaque maiores aliquam veritatis quas. Enim sed aut beatae laborum, nesciunt earum architecto est dolore quos deserunt ut eum explicabo nam error perspiciatis sunt perferendis a ipsa natus, alias maiores illum eius. Eos eligendi eum magnam!</p>
+           
+            <h2 class="bg-dark text-white text-center">OUR FACILITIES</h2>
+            <div class="row">
+                <div class="col-md-6">
+                    <ul class="list-group">
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Expert Faculties <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Personal Attention <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Limited Students in a batch <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Online Progress Report <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Printed notes and question papers <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Pick up and Drop Facility available <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Daily Online Attendance <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Mobile App to keep close control by Parents on Students Performance <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-6">
+                    <ul class="list-group">
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Motivation is the main part of the institute <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Absentees and follow up <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Doubt sessions / Weekly Tests <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Study Room Facility is available <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Main focus on Basics and then go to the Advanced level <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Free test series in two months before exam <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            In person guidance for all Students <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items center">
+                            Reasonable fees <span class="badge badge-primary badge-pill"><i class="fa fa-check"></i></span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card bg-warning text-white">
+                <div class="card-body">
+                    <h4 class="card-title text-center">Parents Review</h4>
+                </div>
+            </div>
+            <img src="images/18.jpg" alt="" class="img-fluid">
+        </div>
+    </div>
 
     <div class="container-fluid">
         <div class="row bg-dark mt-2">
