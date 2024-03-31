2478cf2734593dde48afba719cfbca05cf33fe85
Hamzshaa <trident32000@gmail.com>
2024-03-31 14:11:43 +0300

    gallery page created

---
+++ gallery.php
@@ -0,0 +1,65 @@
+
+<?php
+    include("inc/top.php")
+?>
+
+<div class="container-fluid px-0">
+    <div class="row">
+        <div class="col-lg-12">
+            <?php include("inc/navbar.php") ?>
+        </div>
+    </div>
+
+    <div class="row mt-2">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-3 mt-2">
+                    <img src="images/student/avatar1.jpg" alt="" class="img-fluid" width="250px" height="250px">
+                </div>
+                <div class="col-md-3 mt-2">
+                    <img src="images/student/avatar1.jpg" alt="" class="img-fluid" width="250px" height="250px">
+                </div>
+                <div class="col-md-3 mt-2">
+                    <img src="images/student/avatar1.jpg" alt="" class="img-fluid" width="250px" height="250px">
+                </div>
+                <div class="col-md-3 mt-2">
+                    <img src="images/student/avatar1.jpg" alt="" class="img-fluid" width="250px" height="250px">
+                </div>
+                <div class="col-md-3 mt-2">
+                    <img src="images/student/avatar1.jpg" alt="" class="img-fluid" width="250px" height="250px">
+                </div>
+                <div class="col-md-3 mt-2">
+                    <img src="images/student/avatar1.jpg" alt="" class="img-fluid" width="250px" height="250px">
+                </div>
+                <div class="col-md-3 mt-2">
+                    <img src="images/student/avatar1.jpg" alt="" class="img-fluid" width="250px" height="250px">
+                </div>
+                <div class="col-md-3 mt-2">
+                    <img src="images/student/avatar1.jpg" alt="" class="img-fluid" width="250px" height="250px">
+                </div>
+            </div>
+            <div class="row mt-1">
+                <div class="col-md-12">
+                    <ul class="pagination pagination-circle border-0">
+                        <li class="page-item"><a href="" class="page-link">1</a></li>
+                        <li class="page-item"><a href="" class="page-link">2</a></li>
+                        <li class="page-item"><a href="" class="page-link">3</a></li>
+                        <li class="page-item"><a href="" class="page-link">4</a></li>
+                        <li class="page-item active"><a href="" class="page-link">5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row bg-dark mt-2">
+            <?php include("inc/footer.php") ?>
+        </div>
+    </div>
+</div>
+
+
+
+ </body>
+</html>
